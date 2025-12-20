0badda3b6ac971373d4e85ec706503f990907057
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-20 07:55:41 +0700

    Nambahin rute navigasi untuk halaman Utama (Home)

---
+++ app/src/main/java/com/example/questapi_020/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.questapi_020.uicontroller.route
+
+
+import com.example.questapi_020.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
