93cb66ed819c208854f20284ff8efd32899901eb
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-20 07:54:49 +0700

    Nambahin rute navigasi untuk halaman Entry Siswa

---
+++ app/src/main/java/com/example/questapi_020/uicontroller/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_020.uicontroller.route
+
+import com.example.questapi_020.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
