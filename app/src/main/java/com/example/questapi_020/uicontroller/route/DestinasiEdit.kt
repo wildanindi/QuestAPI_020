f35a42be5639c176164074a6ee376ef95aa05b6d
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-26 18:26:43 +0700

    tambah navigasi edit siswa

--- app/src/main/java/com/example/questapi_020/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_020/uicontroller/route/DestinasiEdit.kt
@@ -1,2 +1,11 @@
 package com.example.questapi_020.uicontroller.route
 
+import com.example.questapi_020.R
+
+object DestinasiEdit : DestinasiNavigasi {
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
