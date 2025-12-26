c9352e372c87040c77381faa1493a1545ae4bbb7
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-26 20:26:54 +0700

    buat sealed interface StatusUIDetail untuk mengelola state UI detail siswa

--- app/src/main/java/com/example/questapi_020/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_020/viewmodel/DetailViewModel.kt
@@ -1,2 +1,31 @@
+@file:OptIn(InternalSerializationApi::class)
+
 package com.example.questapi_020.viewmodel
+
+
+
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_020.modeldata.DataSiswa
+import com.example.questapi_020.repositori.RepositoryDataSiswa
+import com.example.questapi_020.uicontroller.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import kotlinx.serialization.InternalSerializationApi
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+
+sealed interface StatusUIDetail {
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
 
