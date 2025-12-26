4cd803e0b4ade70c3361056f52b058e20e507f23
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-26 18:46:11 +0700

    tambah BarisDetailData buat nampilin label dan isi data secara rapi serta tambah dialog konfirmasi hapus data biar lebih aman

--- app/src/main/java/com/example/questapi_020/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_020/view/HalamanDetail.kt
@@ -190,3 +190,39 @@ fun DetailDataSiswa(
     }
 }
 
+@Composable
+private fun BarisDetailData(
+    @StringRes labelResID: Int,
+    itemDetail: String,
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(stringResource(labelResID))
+        Spacer(modifier = Modifier.width(11.dp))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = { /* Do nothing */ },
+        title = { Text(stringResource(R.string.attention)) },
+        text = { Text(stringResource(R.string.tanya)) },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(stringResource(R.string.no))
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(stringResource(R.string.yes))
+            }
+        }
+    )
+}
