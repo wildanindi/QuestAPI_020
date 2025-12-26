b3050a51322c4448caf4f4bb866de6622dccffc3
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-26 20:27:18 +0700

    implementasi fungsi getSatuSiswa untuk mengambil data siswa berdasarkan ID

--- app/src/main/java/com/example/questapi_020/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_020/viewmodel/DetailViewModel.kt
@@ -28,4 +28,32 @@ sealed interface StatusUIDetail {
     object Loading : StatusUIDetail
 }
 
+class DetailViewModel(savedStateHandle: SavedStateHandle,private val repositoryDataSiswa:
+RepositoryDataSiswa):ViewModel() {
 
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa(){
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(satusiswa = repositoryDataSiswa.getSatuSiswa(idSiswa))
+            }
+            catch (e: IOException){
+                StatusUIDetail.Error
+            }
+            catch (e: HttpException){
+                StatusUIDetail.Error
+            }
+        }
+    }
+
+
+
+}
