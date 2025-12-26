4182da3fe17470e7b82eefb8141c9407fc0852dd
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-26 20:06:31 +0700

    implementasi JaringanRepositoryDataSiswa untuk koneksi ke API

--- app/src/main/java/com/example/questapi_020/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_020/repositori/RepositoryDataSiswa.kt
@@ -26,3 +26,31 @@ interface RepositoryDataSiswa {
     ): retrofit2.Response<Void>
 }
 
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+) : RepositoryDataSiswa {
+
+    override suspend fun getDataSiswa(): List<DataSiswa> =
+        serviceApiSiswa.getSiswa()
+
+    override suspend fun postDataSiswa(
+        dataSiswa: DataSiswa
+    ): retrofit2.Response<Void> =
+        serviceApiSiswa.postSiswa(dataSiswa)
+
+    override suspend fun getSatuSiswa(
+        id: Int
+    ): DataSiswa =
+        serviceApiSiswa.getSatuSiswa(id)
+
+    override suspend fun editSatuSiswa(
+        id: Int,
+        dataSiswa: DataSiswa
+    ): retrofit2.Response<Void> =
+        serviceApiSiswa.editSatuSiswa(id, dataSiswa)
+
+    override suspend fun hapusSatuSiswa(
+        id: Int
+    ): retrofit2.Response<Void> =
+        serviceApiSiswa.hapusSatuSiswa(id)
+}
