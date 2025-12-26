fe661adb5e8e071534e8a6ab0ceb84ac41d2d832
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-26 20:05:46 +0700

    buat interface RepositoryDataSiswa untuk manajemen data siswa

--- app/src/main/java/com/example/questapi_020/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_020/repositori/RepositoryDataSiswa.kt
@@ -5,14 +5,24 @@ import com.example.questapi_020.apiservice.ServiceApiSiswa
 import com.example.questapi_020.modeldata.DataSiswa
 
 interface RepositoryDataSiswa {
+
     suspend fun getDataSiswa(): List<DataSiswa>
-    suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    suspend fun postDataSiswa(
+        dataSiswa: DataSiswa
+    ): retrofit2.Response<Void>
+
+    suspend fun getSatuSiswa(
+        id: Int
+    ): DataSiswa
+
+    suspend fun editSatuSiswa(
+        id: Int,
+        dataSiswa: DataSiswa
+    ): retrofit2.Response<Void>
+
+    suspend fun hapusSatuSiswa(
+        id: Int
+    ): retrofit2.Response<Void>
 }
 
-class JaringanRepositoryDataSiswa(
-    private val serviceApiSiswa: ServiceApiSiswa
-) : RepositoryDataSiswa {
-    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
-    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> =
-        serviceApiSiswa.postSiswa(dataSiswa)
-}
