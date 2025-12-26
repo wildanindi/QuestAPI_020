b40b6aaaf19e7c0b8c20200462a4850c8e74a753
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-26 20:02:38 +0700

    menambah api untuk get per id, put, dan delete

--- app/src/main/java/com/example/questapi_020/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_020/apiservice/ServiceApiSiswa.kt
@@ -3,11 +3,12 @@ package com.example.questapi_020.apiservice
 import com.example.questapi_020.modeldata.DataSiswa
 
 import retrofit2.http.Body
-//import retrofit2.http.DELETE
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
-//import retrofit2.http.PUT
-//import retrofit2.http.Query
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
 
 interface ServiceApiSiswa {
     @GET("bacaTeman.php")
@@ -16,4 +17,19 @@ interface ServiceApiSiswa {
     @POST("insertTM.php")
     suspend fun postSiswa(@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 
+    @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(
+        @Query("id") id: Int
+    ): DataSiswa
+
+    @PUT("editTM.php/{id}")
+    suspend fun editSatuSiswa(
+        @Query("id") id: Int,
+        @Body dataSiswa: DataSiswa
+    ): retrofit2.Response<Void>
+
+    @DELETE("deleteTM.php/{id}")
+    suspend fun hapusSatuSiswa(
+        @Query("id") id: Int
+    ): retrofit2.Response<Void>
 }
