fe2bc969eb3615eefeed6e840225e789f5c34eb5
wildanindi2@gmail.com <wildanindi2@gmail.com>
2025-12-26 18:45:04 +0700

    implementasi BodyDetailDataSiswa untuk handle state UI dan hapus data

--- app/src/main/java/com/example/questapi_020/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_020/view/HalamanDetail.kt
@@ -96,3 +96,52 @@ fun DetailSiswaScreen(
     }
 }
 
+@Composable
+private fun BodyDetailDataSiswa(
+    statusUiDetail: StatusUiDetail,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(
+            dimensionResource(id = R.dimen.padding_medium)
+        ),
+        verticalArrangement = Arrangement.spacedBy(
+            dimensionResource(id = R.dimen.padding_medium)
+        )
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+        when (statusUiDetail) {
+            is StatusUiDetail.Success -> {
+                DetailDataSiswa(
+                    siswa = statusUiDetail.status,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            else -> {}
+        }
+
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(
+                    dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+        }
+    }
+}
+
